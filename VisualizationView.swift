37dd1df7c39a55da7384cae471e16ed7810eb406
松下慎吾 <s-matsushita@ilab.pu-kumamoto.ac.jp>
2022-03-10 19:35:27 +0900

    testViewによる、瞬時の画面遷移完了

--- VisualizationView.swift
+++ VisualizationView.swift
@@ -14,7 +14,6 @@ struct VisualizationView: View {
                 Text("現在の節水料")
                                .font(.largeTitle)
                                .bold()
-                
                 Text("0L")
                 Text("○○月○○日")
                     .padding()
@@ -22,25 +21,57 @@ struct VisualizationView: View {
             }.frame(width: 250, height:160, alignment: .bottomTrailing)
                 .border(Color.red, width: 2) .offset(x:-400,y:-200)
             Image("夜景").clipShape(Circle())
-         
-            Button(action:{
-                //ボタンが押されたときのアクション
-            }){
-                ZStack{
-                    Color.blue
-                        .frame(width:110,height: 110)
-                        .overlay(Text("選ぶ").foregroundColor(.red))
-                }.offset(x:400,y:200)
-            }
+        
+                    NavigationLink(destination: selectView()){
+                        ZStack{
+                            Color.blue
+                                .frame(width:110,height: 110)
+                                .overlay(Text("選ぶ")
+                                .foregroundColor(.red))
+                        }.offset(x:400,y:200)
+                    }
             Text("コップいっぱい")
                 .offset(y:200)
         }
     }
 }
+struct selectView: View{
+    var body: some View{
+        VStack{
+            
+            Text("記録する")
+            HStack{
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+                Image(systemName: "circle")
+            }
+        }
+    }
+}
+struct testView:View{
+    @State private var isShowingView: Bool = false
 
-struct VisualizationView_Previews: PreviewProvider {
+        var body: some View {
+            if isShowingView {
+                selectView()
+            } else {
+                Button {
+                    isShowingView.toggle()
+                } label: {
+                    Text("フラグ切替")
+                }
+            }
+        }
+}
+
+struct testView_Previews: PreviewProvider {
     static var previews: some View {
-        VisualizationView()
+        testView()
 .previewInterfaceOrientation(.landscapeRight)
     }
 }
